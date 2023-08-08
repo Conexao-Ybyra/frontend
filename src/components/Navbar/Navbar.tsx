c1742ee7be69bfefc8e37dadfc92f8e48ef8714e
eliseflaneuse <eliseflaneuse@gmail.com>
2023-08-08 10:47:02 -0300

    Navbar Condicional

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,20 @@
-import { useContext, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../contexts/AuthContext';
-import { toastAlerta } from '../../util/toastAlerta';
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import { toastAlerta } from "../../util/toastAlerta";
 
 function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const { handleLogout } = useContext(AuthContext)
+  const { usuario, handleLogout } = useContext(AuthContext);
 
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
-    function logout() {
-        handleLogout()
-        toastAlerta('Usuário deslogado com sucesso', 'info')
-        navigate('/login')
-    }
+  function logout() {
+    handleLogout();
+    toastAlerta("Usuário deslogado com sucesso", "info");
+    navigate("/login");
+  }
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -24,73 +24,151 @@ function Navbar() {
     setIsDropdownOpen(false);
   };
 
-  return (
-    <header className="ml-8 mr-8 mb-8 flex items-center justify-between border-b py-4 md:mb-12 md:py-8 xl:mb-16">
-      {/* logo - start */}
-      <Link to='/home' className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl" aria-label="logo">Conexão Ybyra</Link>
-      {/* logo - end */}
+  let navbarPos;
+  let navbarPre;
 
-      {/* nav - start */}
-      <nav className={`gap-12 lg:flex ${isDropdownOpen ? 'hidden' : 'lg:block'} lg:items-center lg:w-auto md:hidden sm:hidden `}>
-        
-        <Link to="/postagens" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700" onClick={closeDropdown}>
-          Postagens
-        </Link>
-        <Link to="/cadastroPostagem" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700" onClick={closeDropdown}>
-          Cadastrar Postagem
-        </Link>
-        <Link to="/temas" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700" onClick={closeDropdown}>
-          Temas
-        </Link>
-        <Link to="/cadastroTema" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700" onClick={closeDropdown}>
-          Cadastrar Temas
-        </Link>
-        <Link to="/perfil" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700" onClick={closeDropdown}>
-          Perfil
-        </Link>
-        <Link to="/about" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700" onClick={closeDropdown}>
-          Sobre
-        </Link>
-      </nav>
-      {/* nav - end */}
-
-      {/* buttons - start */}
-      <button onClick={logout} className="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block">Sair</button>
-
-      {/* Dropdown menu for smaller screens */}
-      <div className="relative lg:hidden">
-        <button
-          type="button"
-          className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden"
-          onClick={toggleDropdown}
+  if (usuario.token !== "") {
+    navbarPos = (
+      <header className="ml-8 mr-8 mb-8 flex items-center justify-between border-b py-4 md:mb-12 md:py-8 xl:mb-16">
+        {/* logo - start */}
+        <Link
+          to="/home"
+          className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl"
+          aria-label="logo"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
-          </svg>
-          Menu
+          Conexão Ybyra
+        </Link>
+        {/* logo - end */}
+
+        {/* nav - start */}
+        <nav
+          className={`gap-12 lg:flex ${
+            isDropdownOpen ? "hidden" : "lg:block"
+          } lg:items-center lg:w-auto md:hidden sm:hidden `}
+        >
+          <Link
+            to="/postagens"
+            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            onClick={closeDropdown}
+          >
+            Postagens
+          </Link>
+          <Link
+            to="/cadastroPostagem"
+            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            onClick={closeDropdown}
+          >
+            Cadastrar Postagem
+          </Link>
+          <Link
+            to="/temas"
+            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            onClick={closeDropdown}
+          >
+            Temas
+          </Link>
+          <Link
+            to="/cadastroTema"
+            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            onClick={closeDropdown}
+          >
+            Cadastrar Temas
+          </Link>
+          <Link
+            to="/perfil"
+            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            onClick={closeDropdown}
+          >
+            Perfil
+          </Link>
+          <Link
+            to="/about"
+            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            onClick={closeDropdown}
+          >
+            Sobre
+          </Link>
+        </nav>
+        {/* nav - end */}
+
+        {/* buttons - start */}
+        <button
+          onClick={logout}
+          className="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block"
+        >
+          Sair
         </button>
 
-        {isDropdownOpen && (
-          <div className="absolute right-0 mt-2 w-40 rounded-lg bg-white shadow-lg">
-            <Link to="/postagens" className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-indigo-100" onClick={closeDropdown}>
-              Postagens
-            </Link>
-            <Link to="/temas" className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-indigo-100" onClick={closeDropdown}>
-              Temas
-            </Link>
-            <Link to="/cadastroTema" className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-indigo-100" onClick={closeDropdown}>
-              Cadastrar Temas
-            </Link>
-            <Link to="/perfil" className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-indigo-100" onClick={closeDropdown}>
-              Perfil
-            </Link>
-          </div>
-        )}
-      </div>
-      {/* Dropdown menu for smaller screens - end */}
-      {/* buttons - end */}
-    </header>
-  );
-};
+        {/* Dropdown menu for smaller screens */}
+        <div className="relative lg:hidden">
+          <button
+            type="button"
+            className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden"
+            onClick={toggleDropdown}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Menu
+          </button>
+
+          {isDropdownOpen && (
+            <div className="absolute right-0 mt-2 w-40 rounded-lg bg-white shadow-lg">
+              <Link
+                to="/postagens"
+                className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-indigo-100"
+                onClick={closeDropdown}
+              >
+                Postagens
+              </Link>
+              <Link
+                to="/temas"
+                className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-indigo-100"
+                onClick={closeDropdown}
+              >
+                Temas
+              </Link>
+              <Link
+                to="/cadastroTema"
+                className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-indigo-100"
+                onClick={closeDropdown}
+              >
+                Cadastrar Temas
+              </Link>
+              <Link
+                to="/perfil"
+                className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-indigo-100"
+                onClick={closeDropdown}
+              >
+                Perfil
+              </Link>
+            </div>
+          )}
+        </div>
+        {/* Dropdown menu for smaller screens - end */}
+        {/* buttons - end */}
+      </header>
+    );
+  } else if (usuario.token == "") {
+    navbarPre = (<h1>Hello World</h1>);
+
+  }
+
+  return (
+    <>
+    {navbarPos}
+    {navbarPre}
+    </>
+    ) ;
+}
 
 export default Navbar;
