eb11400b88572d5d2670991d824b9702b7cd6042
Elise Flaneuse <eliseflaneuse@gmail.com>
2023-08-11 11:14:54 -0300

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
-import { toastAlerta } from "../../util/toastAlerta";
 import ybyra from '../../assets/images/cy-logo.png';
 
 function Navbar() {
