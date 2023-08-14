35b8b939960e0a4eaf0cfee1ff4c067f762634ca
Elise Flaneuse <eliseflaneuse@gmail.com>
2023-08-14 12:24:58 -0300

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -111,14 +111,14 @@ function Navbar() {
             <div className="absolute right-0 mt-2 w-40 rounded-lg bg-white shadow-lg">
               <Link
                 to="/home"
-                className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-english-violet"
+                className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-english-violet/50"
                 onClick={closeDropdown}
               >
                 Home
               </Link>
               <Link
                 to="/sobre"
-                className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:bg-english-violet"
+                className="block px-4 py-2 text-sm text-gray-600 font-semibold hover:text-white hover:bg-english-violet"
                 onClick={closeDropdown}
               >
                 Sobre
