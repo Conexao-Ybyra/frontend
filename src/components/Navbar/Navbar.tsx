3b8886434edb00c7e3bc261a9a00d52b9c8bd09a
eliseflaneuse <eliseflaneuse@gmail.com>
2023-08-14 12:15:09 -0300

    Botões de entrar e cadastre-se no dorpdown menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -82,7 +82,7 @@ function Navbar() {
             className="hidden rounded-3xl bg-orange-crayola px-5 py-3 mr-3 text-center text-sm font-semibold text-antiflash-white outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 hover:text-orange-crayola focus-visible:ring active:text-gray-700 md:text-base lg:inline-block"
           >
             Cadastre-se
-            </Link>
+          </Link>
         </div>
 
         {/* Dropdown menu for smaller screens */}
@@ -137,6 +137,21 @@ function Navbar() {
               >
                 Contato
               </Link>
+              <div>
+                <Link
+                  to="/login"
+                  className="hidden rounded-3xl bg-orange-crayola px-5 py-3 mr-3 text-center text-sm font-semibold text-antiflash-white outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 hover:text-orange-crayola focus-visible:ring active:text-gray-700 md:text-base lg:inline-block"
+                >
+                  Entrar
+                </Link>
+
+                <Link
+                  to="/cadastro"
+                  className="hidden rounded-3xl bg-orange-crayola px-5 py-3 mr-3 text-center text-sm font-semibold text-antiflash-white outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 hover:text-orange-crayola focus-visible:ring active:text-gray-700 md:text-base lg:inline-block"
+                >
+                  Cadastre-se
+                </Link>
+              </div>
             </div>
           )}
         </div>
